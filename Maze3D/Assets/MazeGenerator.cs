5eaebef2d45a6e11b6165fd9eba82262bb86709d
Boris Dmitrochenko <kubsia@mail.ru>
2022-12-10 12:59:16 +0300

    Added 3D Maze and movable player, added comments in code

--- Maze3D/Assets/MazeGenerator.cs
+++ Maze3D/Assets/MazeGenerator.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MazeGeneratorCell
+public class MazeGeneratorCell //массив ячеек, который вернется в качестве лабиринта
 {
     public int X;
     public int Y;
 
-    public bool WallLeft = true;
-    public bool WallBottom = true;
+    public bool WallLeft = true; //все стены по умолчанию включены
+    public bool WallBottom = true; //а затем алгоритм будет удалять стены с помощью recursive backtracking
 
     public bool Visited = false;
     public int DistanceFromStart;
@@ -19,7 +19,7 @@ public class MazeGenerator
     public int Width = 23;
     public int Height = 15;
 
-    public MazeGeneratorCell[,] GenerateMaze()
+    public MazeGeneratorCell[,] GenerateMaze() //создаем двумерный массив ячеек
     {
         MazeGeneratorCell[,] maze = new MazeGeneratorCell[Width, Height];
 
@@ -27,10 +27,11 @@ public class MazeGenerator
         {
             for (int y = 0; y<maze.GetLength(1); y++)
             {
-                maze[x, y] = new MazeGeneratorCell { X = x, Y = y };
+                maze[x, y] = new MazeGeneratorCell { X = x, Y = y }; //заполнение массива нулями
             }
         }
 
+        //убираем лишние стены сверху и справа
         for (int x = 0; x < maze.GetLength(0); x++)
         {
             maze[x, Height - 1].WallLeft = false;
@@ -48,41 +49,42 @@ public class MazeGenerator
         return maze;
     }
 
-    private void RemoveWallsWithBacktracker(MazeGeneratorCell[,] maze)
+    private void RemoveWallsWithBacktracker(MazeGeneratorCell[,] maze) //удаление стен из лабиринта
     {
         MazeGeneratorCell current = maze[0, 0];
         current.Visited = true;
         current.DistanceFromStart = 0;
 
-        Stack<MazeGeneratorCell> stack = new Stack<MazeGeneratorCell>();
+        Stack<MazeGeneratorCell> stack = new Stack<MazeGeneratorCell>(); //список ячеек, через который мы прошли в ходе случайного блуждания
         do
         {
-            List<MazeGeneratorCell> unvisitedNeighbours = new List<MazeGeneratorCell>();
+            List<MazeGeneratorCell> unvisitedNeighbours = new List<MazeGeneratorCell>(); //список всех ячеек, в которых мы еще не были
 
             int x = current.X;
             int y = current.Y;
 
+            //проверка посещенности - если не посещена, вносим в список (при этом ячейка не должна быть крайней)
             if (x > 0 && !maze[x - 1, y].Visited) unvisitedNeighbours.Add(maze[x-1, y]);
             if (y > 0 && !maze[x, y - 1].Visited) unvisitedNeighbours.Add(maze[x, y - 1]);
             if (x < Width - 2 && !maze[x + 1, y].Visited) unvisitedNeighbours.Add(maze[x + 1, y]);
             if (y < Height - 2 && !maze[x, y + 1].Visited) unvisitedNeighbours.Add(maze[x, y + 1]);
 
-            if (unvisitedNeighbours.Count > 0)
+            if (unvisitedNeighbours.Count > 0) //выбираем случайного соседа, куда пойдем
             {
                 MazeGeneratorCell chosen = unvisitedNeighbours[UnityEngine.Random.Range(0, unvisitedNeighbours.Count)];
-                RemoveWall(current, chosen);
+                RemoveWall(current, chosen); //сносим стену между текущей и выбранной ячейкой
 
-                chosen.Visited = true;
-                stack.Push(chosen);
-                current = chosen;
-                chosen.DistanceFromStart = stack.Count;
+                chosen.Visited = true; //отмечаем ячейку, чтобы еще раз через неё не пойти
+                stack.Push(chosen); //добавляем её в список посещенных ячеек
+                current = chosen; //и делаем активной
+                chosen.DistanceFromStart = stack.Count; //массив дистанции от начала, создан для генерации выхода
             }
             else
             {
-                current = stack.Pop();
+                current = stack.Pop(); //при обратном трэкинге возвращаемся назад и удаляем ячейку из списка
             }
 
-        } while (stack.Count > 0);
+        } while (stack.Count > 0); //значение 0 означает, что мы вернулись в начало, тем самым построили лабиринт
     }
 
     private void RemoveWall(MazeGeneratorCell a, MazeGeneratorCell b)
@@ -103,6 +105,7 @@ public class MazeGenerator
     {
         MazeGeneratorCell furthest = maze[0,0];
 
+        //ищем самую дальнюю ячейку на краю лабиринта
         for (int x = 0; x < maze.GetLength(0); x++)
         {
             if (maze[x, Height - 2].DistanceFromStart > furthest.DistanceFromStart) furthest = maze[x, Height - 2];
@@ -115,6 +118,7 @@ public class MazeGenerator
             if (maze[0, y].DistanceFromStart > furthest.DistanceFromStart) furthest = maze[0, y];
         }
 
+        //удаляем стену ячейки для создания выхода
         if (furthest.X == 0) furthest.WallLeft = false;
         else if (furthest.Y == 0) furthest.WallBottom = false;
         else if (furthest.X == Width - 2) maze[furthest.X + 1, furthest.Y].WallLeft = false;
--- Maze3D/Assets/MazeSpawner.cs
+++ Maze3D/Assets/MazeSpawner.cs
@@ -6,6 +6,8 @@ public class MazeSpawner : MonoBehaviour
 {
     public GameObject CellPrefab;
 
+    public Vector3 CellSize = new Vector3(1,1,0);
+
     void Start()
     {
         MazeGenerator generator = new MazeGenerator();
@@ -15,9 +17,9 @@ public class MazeSpawner : MonoBehaviour
         {
             for (int y = 0; y < maze.GetLength(1); y++)
             {
-                Cell c = Instantiate(CellPrefab, new Vector2(x, y), Quaternion.identity).GetComponent<Cell>();
+                Cell c = Instantiate(CellPrefab, new Vector3(x * CellSize.x, y * CellSize.y, y * CellSize.z), Quaternion.identity).GetComponent<Cell>();
 
-                c.WallLeft.SetActive(maze[x, y].WallLeft);
+                c.WallLeft.SetActive(maze[x, y].WallLeft); //делаем стену активной, если такая стена есть в переданном массиве
                 c.WallBottom.SetActive(maze[x, y].WallBottom);
             }
         }
